a1afb84255c94b7c369483434e9c6c8dc06eb2eb
shimakaze-git <shimakaze.soft+github@googlemail.com>
2020-08-21 04:38:17 +0900

    refactor: autido部分のフロント部分を修正.

--- audio/public/javascripts/client.js
+++ audio/public/javascripts/client.js
@@ -2,12 +2,86 @@ const socket = io.connect()
 let processor = null
 let localstream = null
 
+let num = 0
+
+// サンプリング周波数
+let sampleRate = 0
+
+// 期間
+let duration = 0.0
+
+// AudioContextはすべての音声の再生を管理
+context = new window.AudioContext()
+
+function send_pcm(voice) {
+    // socket.emit('send_pcm', voice.buffer)
+}
+
+function availableData(arr) {
+    var b = false;
+    for (var i = 0; i < arr.length && !b; i++) {
+        b = (arr[i] != 0);
+    }
+    return b;
+}
+
+function handleSuccess(stream) {
+    console.log('stream')
+
+    localstream = stream
+
+    // var source = context.createBufferSource();
+    const input = context.createMediaStreamSource(stream);
+    processor = context.createScriptProcessor(1024, 1, 1);
+
+    // window.dotnsf_hack_for_mozzila = input;
+    input.connect(processor)
+    // processor.connect(context.destination)
+
+    processor.onaudioprocess = function (e) {
+        // 音声データ
+        let inputdata = e.inputBuffer.getChannelData(0);
+
+        if (!num) {
+            // バッファに格納されたPCMデータのチャンネルの数をintegerで返す
+            num = e.inputBuffer.numberOfChannels;
+            floatData = new Array(num);
+
+            console.log('num', num)
+
+            for (var i = 0; i < num; i++) {
+                floatData[i] = [];
+            }
+            sampleRate = e.inputBuffer.sampleRate;
+        }
+
+        // let float32Array = e.inputBuffer.getChannelData(0);
+        let float32Array = inputdata
+        if (availableData(float32Array)) {
+            duration += e.inputBuffer.duration;
+            length += e.inputBuffer.length;
+
+            for (var i = 0; i < num; i++) {
+                float32Array = e.inputBuffer.getChannelData(i);
+                Array.prototype.push.apply(floatData[i], float32Array);
+            }
+        }
+
+        // 音声データを送る
+        send_pcm(inputdata)
+    }
+    processor.connect(context.destination);
+}
+
 function startRecording() {
+
+    // $('#recBtn').css('display', 'none');
+    // $('#stopBtn').css('display', 'block');
 
     console.log('start recording')
 
     // AudioContextはすべての音声の再生を管理
-    context = new window.AudioContext()
+    // context = new window.AudioContext()
 
     // サンプリング周波数を start を指定してサーバに送信
     socket.emit('start', {
@@ -22,31 +96,25 @@ function startRecording() {
     navigator.mediaDevices.getUserMedia({
         audio: true,
         video: false
-    }).then((stream) => {
-        console.log('stream')
-
-        localstream = stream
-        const input = this.context.createMediaStreamSource(stream)
-        processor = context.createScriptProcessor(4096, 1, 1)
-
-        input.connect(processor)
-        processor.connect(context.destination)
-
-        processor.onaudioprocess = (e) => {
-            const voice = e.inputBuffer.getChannelData(0)
-            console.log(voice.buffer)
-            socket.emit('send_pcm', voice.buffer)
-        }
-    }).catch((e) => {
+    }).then(handleSuccess).catch((e) => {
         console.log(e)
     })
 }
 
 function stopRecording() {
+    // $('#recBtn').css( 'display', 'block' );
+    // $('#stopBtn').css( 'display', 'none' );
+
     console.log('stop recording')
-    processor.disconnect()
-    processor.onaudioprocess = null
-    processor = null
+    if (processor) {
+        processor.disconnect()
+        processor.onaudioprocess = null
+        processor = null
+    }
+
+    // processor.disconnect()
+    // processor.onaudioprocess = null
+    // processor = null
 
     localstream.getTracks().forEach((track) => {
         track.stop()
