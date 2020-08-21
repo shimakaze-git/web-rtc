156ec601051c68c279243ec221bffb7c40a1cdef
shimakaze-git <shimakaze.soft+github@googlemail.com>
2020-08-21 17:43:25 +0900

    refactor: chartの生成.

--- audio/public/javascripts/client.js
+++ audio/public/javascripts/client.js
@@ -10,8 +10,11 @@ let sampleRate = 0
 // 期間
 let duration = 0.0
 
+// 長さ
+let length = 0
+
 // AudioContextはすべての音声の再生を管理
-context = new window.AudioContext()
+let context = null
 
 function send_pcm(voice) {
     // socket.emit('send_pcm', voice.buffer)
@@ -47,8 +50,6 @@ function handleSuccess(stream) {
             num = e.inputBuffer.numberOfChannels;
             floatData = new Array(num);
 
-            console.log('num', num)
-
             for (var i = 0; i < num; i++) {
                 floatData[i] = [];
             }
@@ -81,7 +82,7 @@ function startRecording() {
     console.log('start recording')
 
     // AudioContextはすべての音声の再生を管理
-    // context = new window.AudioContext()
+    context = new window.AudioContext()
 
     // サンプリング周波数を start を指定してサーバに送信
     socket.emit('start', {
@@ -102,27 +103,94 @@ function startRecording() {
 }
 
 function stopRecording() {
-    // $('#recBtn').css( 'display', 'block' );
-    // $('#stopBtn').css( 'display', 'none' );
-
-    console.log('stop recording')
-    if (processor) {
-        processor.disconnect()
-        processor.onaudioprocess = null
-        processor = null
-    }
-
-    // processor.disconnect()
-    // processor.onaudioprocess = null
-    // processor = null
+    processor.disconnect()
+    processor.onaudioprocess = null
+    processor = null
 
     localstream.getTracks().forEach((track) => {
         track.stop()
     })
 
-    socket.emit('stop', '', (res) => {
-        console.log(`Audio data is saved as ${res.filename}`)
-    })
+    // socket.emit('stop', '', (res) => {
+    //     console.log(`Audio data is saved as ${res.filename}`)
+    // })
+}
+
+function create_signal_waveform() {
+    // $('#recBtn').css( 'display', 'block' );
+    // $('#stopBtn').css( 'display', 'none' );
+
+    if (processor) {
+        processor.disconnect()
+        processor.onaudioprocess = null
+        processor = null
+
+        localstream.getTracks().forEach((track) => {
+            track.stop()
+        })
+
+        let audioBuffer = context.createBuffer(num, length, sampleRate);
+        // console.log('audioBuffer', audioBuffer)
+
+        for (var i = 0; i < num; i++) {
+            audioBuffer.getChannelData(i).set(floatData[i]);
+        }
+
+        // .これを再生する
+        console.log(audioBuffer);
+        let source = context.createBufferSource();
+
+        source.buffer = audioBuffer; //. オーディオデータの実体（AudioBuffer インスタンス）
+        source.loop = false; //. ループ再生するか？
+        source.loopStart = 0; //. オーディオ開始位置（秒単位）
+        source.loopEnd = audioBuffer.duration; //. オーディオ終了位置（秒単位）
+        source.playbackRate.value = 1.0; //. 再生速度＆ピッチ
+
+        source.connect(context.destination);
+
+        //. for lagacy browsers
+        source.start(0);
+        source.onended = function (event) {
+            //. イベントハンドラ削除
+            source.onended = null;
+            document.onkeydown = null;
+            num = 0;
+            duration = 0.0;
+            length = 0;
+
+            //. オーディオ終了
+            source.stop(0);
+
+            console.log('audio stopped.');
+        };
+
+        //. floatData[] （の先頭の一部）をグラフ描画する
+        let dotnum = 1024;
+        let ctx = document.getElementById('myChart').getContext('2d');
+        let labels = [];
+        let datasets = [];
+        let colors = ["rgba( 255, 0, 0, 0.4 )", "rgba( 0, 0, 255, 0.4 )"];
+
+        for (let i = 0; i < dotnum; i++) {
+            labels.push("" + (i + 1));
+        }
+
+        for (let i = 0; i < num; i++) {
+            datasets.push({
+                label: "data " + i,
+                data: floatData[i].slice(1024, 1024 + dotnum),
+                backgroundColor: colors[i % 2]
+            });
+        }
+
+        let myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: datasets
+            }
+        });
+    }
 }
 
 let localMediaStream = null
